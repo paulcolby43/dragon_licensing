901cf77095b722a5a19f4dfba2ad3fc7a2481db5
Jeremy <senn.jeremy@gmail.com>
2021-04-28 06:08:32 -0700

    License csv file download should make empty strings if no data

--- app/models/license.rb
+++ app/models/license.rb
@@ -65,15 +65,15 @@ class License < ApplicationRecord
     require 'csv'
     private_key = '3f~4Os^8}a5%+fRp'
     device_count = self.number_of_devices.to_s unless self.number_of_devices.blank?
-    number_of_sites = self.number_of_subnets.to_s unless self.number_of_subnets.blank?
-    number_of_cameras = self.number_of_cameras.to_s unless self.number_of_cameras.blank?
+    number_of_sites = self.number_of_subnets.blank? ? '0' : self.number_of_subnets.to_s
+    number_of_cameras = self.number_of_cameras.blank? ? '0' : self.number_of_cameras.to_s
     name = self.account.YardName unless self.account.blank?
-    account_number = self.account.AccountNumber unless self.account.blank?
-    expire_date = self.ExpireDate.strftime('%-m/%-d/%Y') unless self.ExpireDate.blank?
-    ezcash_mac = self.ezcash_mac_address.gsub(" ", "").strip.downcase unless self.ezcash_mac_address.blank?
-    ezcash_mac_converted = self.ezcash_mac_address.gsub("-", "").gsub(" ", "").gsub(":", "").strip.downcase unless self.ezcash_mac_address.blank?
-    jpegger_mac = self.jpegger_mac_address.gsub(" ", "").strip.downcase unless self.jpegger_mac_address.blank?
-    jpegger_mac_converted = self.jpegger_mac_address.gsub("-", "").gsub(" ", "").gsub(":", "").strip.downcase unless self.jpegger_mac_address.blank?
+    account_number = self.account.blank? ? '' : self.account.AccountNumber 
+    expire_date = self.ExpireDate.blank? ? '' : self.ExpireDate.strftime('%-m/%-d/%Y')
+    ezcash_mac = self.ezcash_mac_address.blank? ? '' : self.ezcash_mac_address.gsub(" ", "").strip.downcase
+    ezcash_mac_converted = self.ezcash_mac_address.blank? ? '' : self.ezcash_mac_address.gsub("-", "").gsub(" ", "").gsub(":", "").strip.downcase
+    jpegger_mac = self.jpegger_mac_address.blank? ? '' : self.jpegger_mac_address.gsub(" ", "").strip.downcase
+    jpegger_mac_converted = self.jpegger_mac_address.blank? ? '' : self.jpegger_mac_address.gsub("-", "").gsub(" ", "").gsub(":", "").strip.downcase
     CSV.generate(headers: false, col_sep: ";", row_sep: ";\r\n") do |csv|
       
       unless number_of_devices.blank?
